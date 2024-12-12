3709b131b628696d94cec0a398a7ee973841120c
Berlin Tejeda <berlintejeda22@gmail.com>
2024-12-12 11:09:44 -0500

    displayed all starships

--- star wars API/src/App.jsx
+++ star wars API/src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { getAllStarships } from "./services/sw-api";
+import StarshipCard from "./StarshipCard";
 
 function App() {
-  return <></>;
+  const [starships, setStarships] = useState([]);
+
+  useEffect(() => {
+    const fetchStarships = async () => {
+      const starshipData = await getAllStarships();
+      setStarships(starshipData);
+    };
+
+    fetchStarships();
+  }, []);
+
+  return (
+    <div>
+      <h1>Starships</h1>
+      <div>
+        {starships.map((starship, index) => (
+          <StarshipCard key={index} starship={starship} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ star wars API/src/services/sw-api.jsx
@@ -0,0 +1,10 @@
+export async function getAllStarships() {
+  try {
+    const response = await fetch("https://swapi.dev/api/starships");
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching starships:", error);
+    return [];
+  }
+}
